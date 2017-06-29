94217123e2d22ccf870df54c55bbc59ddd4547fd
adace123 <rexforddrive@gmail.com>
2017-06-29 02:24:08 -0700

    Update packageform.php

--- packageform.php
+++ packageform.php
@@ -3,7 +3,6 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css"/>
 	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
-	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<style>
