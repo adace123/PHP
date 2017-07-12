e97e0704ac4a14f413145a059b1ed3d4e7b251e8
Aaron <rexforddrive@gmail.com>
2017-07-12 04:07:06 -0700

    Create results.php

---
+++ calculator/results.php
@@ -0,0 +1,64 @@
+<?php
+class Calculator{
+    private $_intNum1;
+    private $_intNum2;
+
+    public function __construct($intNum1, $intNum2){
+        $this->_intNum1 = $intNum1;
+        $this->_intNum2 = $intNum2;
+    }
+
+    public function firstNum(){
+        return $this->_intNum1;
+    }
+
+    public function secondNum(){
+        return $this->_intNum2;
+    }
+
+    public function add(){
+        return $this->_intNum1 + $this->_intNum2;
+    }
+
+    public function subtract(){
+        return $this->_intNum1 - $this->_intNum2;
+    }
+
+    public function multiply(){
+        return $this->_intNum1 * $this->_intNum2;
+    }
+
+    public function divide(){
+        return $this->_intNum1 / $this->_intNum2;
+    }
+
+}
+
+if(isset($_REQUEST['results'])){
+   $expression = explode(" ",$_REQUEST['results']);
+   $calc = new Calculator($expression[0],$expression[2]);
+  
+  if(count($expression) == 3)
+   switch($expression[1]){
+       case "+":
+        echo $calc->add();
+        break;
+       case "-":
+        echo $calc->subtract();
+        break;
+       case "*":
+        echo $calc->multiply();
+        break;
+       case "/":
+        echo $calc->divide();
+        break;
+   }   
+ else {
+     $str = "";
+     foreach($expression as $field => $value){
+         $str .= $value;
+     }
+     echo eval("return $str;");
+ }
+}
+?>
